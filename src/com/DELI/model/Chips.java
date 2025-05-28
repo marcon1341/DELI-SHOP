3beac51ee63209d96a7d960f2caf58fdd3df5956
marcon <marconsolomonyadete@gmail.com>
2025-05-27 20:30:11 -0700

    Added predefined chips option using List.

--- src/com/DELI/model/Chips.java
+++ src/com/DELI/model/Chips.java
@@ -1,5 +1,7 @@
 package com.DELI.model;
 
+import java.util.List;
+
 public class Chips implements PricedItem {
     private final String flavour;
     private static  final double price = 1.50;
@@ -8,6 +10,8 @@ public class Chips implements PricedItem {
         this.flavour = flavour.toUpperCase();
     }
 
+    //Chips flavour
+    public static final List<String> chipsOption = List.of("BBQ", "Sour Cream", "Salt & Vinegar", "Jalapeño", "Lays Classic");
     @Override
     public double getPrice() {
         return price;
