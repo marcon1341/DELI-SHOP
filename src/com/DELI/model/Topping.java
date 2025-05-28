09dc1ffd2f5427a8e9e3aa6e92d2dfdbc9837c88
marcon <marconsolomonyadete@gmail.com>
2025-05-27 20:25:20 -0700

    edit Topping class by include predefined bread type and topping options using List and Map.

--- src/com/DELI/model/Topping.java
+++ src/com/DELI/model/Topping.java
@@ -1,35 +1,42 @@
 package com.DELI.model;
 
+import java.util.List;
+import java.util.Map;
+
 public class Topping {
     private  final String category;
     private final String name;
-
-    //
-    public static final String meat = "MEAT";
-    public static final String extraMeat = "EXTRA MEAT";
-    public static final String cheese = "CHEESE";
-    public static final String extraCheese = "EXTRA CHEESE";
-    public static final String regular = "REGULAR";
-    public static final String sauce = "SAUCE";
-    public static final String side = "SIDE";
 
     public Topping(String category, String name) {
         this.category = category.toUpperCase();
         this.name = name;
     }
+    //bread option
+    public static final List<String> breadOptions = List.of("WHITE", "WHEAT", "RYE", "WRAP", "CIABATTA");
+
+    //topping options
+    public static final Map<String, List<String>> options = Map.of(
+            "MEAT", List.of("Steak", "Ham", "Salami","Pastrami", "Roast Beef", "Chicken", "Bacon"),
+            "EXTRA MEAT", List.of("Steak", "Ham", "Salami","Pastrami", "Roast Beef", "Chicken", "Bacon"),
+            "CHEESE", List.of("American", "Provolone", "Cheddar", "Swiss"),
+            "EXTRA CHEESE", List.of("American", "Provolone", "Cheddar", "Swiss"),
+            "REGULAR", List.of("Lettuce", "Peppers", "Onions", "Tomatoes", "Jalapeños", "Cucumbers", "Pickles", "Guacamole", "Mushrooms"),
+            "SAUCE", List.of("Mayo", "Mustard", "Ketchup", "Ranch", "Thousand Islands", "Vinaigrette"),
+            "SIDE", List.of("Au Jus", "Sauce")
+    );
 
     public double getPrice(int sandwichSize){
         if(sandwichSize != 4 && sandwichSize != 8 && sandwichSize != 12){
             throw new IllegalArgumentException("Size must be 4, 8, or 12 inches");
         }
         switch (category){
-            case meat:
+            case "MEAT":
                 return (sandwichSize == 4 ? 1.00 :sandwichSize == 8 ? 2.00 :3.00);
-            case extraMeat:
+            case "EXTRA MEAT":
                 return (sandwichSize == 4 ? 0.50 :sandwichSize == 8 ? 1.00 : 1.50);
-            case cheese:
+            case "CHEESE":
                 return (sandwichSize == 4 ? 0.75 :sandwichSize == 8 ? 1.50 : 2.25);
-            case extraCheese:
+            case "EXTRA CHEESE":
                 return (sandwichSize == 4 ? 0.30 : sandwichSize == 8 ? 0.60 : 0.90);
             default:
                 return 0.0;
