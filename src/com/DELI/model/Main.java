cf381b8e4dc0bd3f01e300817e1d7327737ddebe
marcon <marconsolomonyadete@gmail.com>
2025-05-28 19:21:14 -0700

    Add menu class for cli nafigation and order creation including sandwich, drink, chips and special sandwich handling.

---
+++ src/com/DELI/model/Main.java
@@ -0,0 +1,9 @@
+package com.DELI.model;
+
+public class Main {
+    public static void main(String[] args) {
+        Symbol.printBanner();
+        Menu menu = new Menu();
+        menu.homeScreen();
+    }
+}
