8f1e5c1faf21cb9a23697ea1e3813c120aee1d28
marcon <marconsolomonyadete@gmail.com>
2025-05-29 19:19:26 -0700

    Documentation and comment added.

--- src/com/DELI/model/PricedItem.java
+++ src/com/DELI/model/PricedItem.java
@@ -1,5 +1,10 @@
 package com.DELI.model;
 
+/**
+ * this interface represent any item that has price can provide its price
+ * and must define getPrice()
+ */
 public interface PricedItem {
+    //return the price of item
     double getPrice();
 }
